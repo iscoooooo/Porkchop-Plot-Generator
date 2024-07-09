6366fe6fec9fd3e5284a351e2a6ddfd44a9e82fd
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-07-09 15:22:41 -0700

    Modifed interplanetary_porkchop. Still need to solve ephemeris output issue

--- src/porkchop.py
+++ src/porkchop.py
@@ -16,55 +16,95 @@ def interplanetary_porkchop( config ):
     
     # Default config dictionary
     _config = {
-        'planet0'       : pd.earth[ 'ID' ],
-        'planet1'       : pd.mars[ 'ID' ],
-        'departure0'    : '2020-07-01',
-        'departure1'    : '2020-09-01',
-        'arrival0'      : '2020-11-01',
-        'arrival1'      : '2022-01-24',
-        'mu'            : pd.sun[ 'mu' ],
-        'step'          : 1,
-        'frame'         : 'J2000',
-        'observer'      : '500@0',
-        'cutoff_v'      : 20.0,
-        'c3_levels'     : None,
-        'vinf_levels'   : None,
-        'tof_levels'    : None,
-        'dv_levels'     : None,
-        'dv_cmap'       : 'RdPu_r',
-        'figsize'       : ( 20, 10 ),
-        'lw'            : 1.5,
-        'title'         : 'Porkchop Plot',
-        'fontsize'      : 15,
-        'show'          : False,
-        'filename'      : None,
-        'filename_dv'   : None,
-        'dpi'           : 300,
-        'load'          : False
+        'planet0'       : pd.earth[ 'ID' ],     # Departure planet
+        'planet1'       : pd.mars[ 'ID' ],      # Target planet
+        'departure0'    : '2020-07-01',         # Intial departure date
+        'departure1'    : '2020-09-01',         # Final departure date
+        'arrival0'      : '2020-11-01',         # Initial arrival date
+        'arrival1'      : '2022-01-24',         # Final arrival date
+        'mu'            : pd.sun[ 'mu' ],       # Gravitational parameter in km**3/s**2
+        'step'          : 1,                    # Step size in days
+        'frame'         : 'J2000',              # Ecliptic of J2000
+        'observer'      : '500@0',              # Solar Sytem Barycenter
+        'cutoff_v'      : 20.0,                 # Maximum vinf to consider             
+        'c3_levels'     : None,                 # C3 levels for contour plot
+        'vinf_levels'   : None,                 # vinf levels for contour plot
+        'tof_levels'    : None,                 # tof levels for contour plot
+        'dv_levels'     : None,                 # dv levels for contour plot
+        'dv_cmap'       : 'RdPu_r',             # color map for dv contours
+        'figsize'       : ( 20, 10 ),           # figure size for contour plot
+        'lw'            : 1.5,                  # linewidth for contour lines
+        'title'         : 'Porkchop Plot',      # Plot title
+        'fontsize'      : 15,                   # Axes fontsize
+        'show'          : False,                # For displaying the figure
+        'filename'      : None,                 # Specify filename for c3 plot
+        'filename_dv'   : None,                 # Specify filename for dv plot
+        'dpi'           : 300,                  # Specify target dpi
+        'load'          : False                 #
     }
     
     # Overrides default config parameters
     for key in config.keys():
         _config[ key ] = config [ key ]
 
-    # Compute cutoff C3
+    # Define cutoff C3
     cutoff_c3 = _config[ 'cutoff_v' ] ** 2
 
-    # Arrays of departure and arrival times
+    # Generate URLs for querying ephemeris data from Horizons API
+    url_departure = generate_url(
+        _config[ 'planet0' ],
+        _config[ 'departure0' ],
+        _config[ 'departure1' ],
+        _config[ 'step' ]
+    ) 
+    url_arrival   = generate_url(
+        _config[ 'planet1' ],
+        _config[ 'arrival0' ],
+        _config[ 'arrival1' ],
+        _config[ 'step' ]
+    )
 
+    # Define target output path for departure and arrival data
+    departure_output_path = f"{_config[ 'planet0' ]}_{_config[ 'departure0' ]}_{_config[ 'departure1']}.txt"
+    arrival_output_path = f"{_config[ 'planet1' ]}_{_config[ 'arrival0' ]}_{_config[ 'arrival1']}.txt"
+
+    # Submit API request and save the response to text files in target paths
+    save_query_to_file(
+        url_departure,
+        departure_output_path
+    )
+    save_query_to_file(
+        url_arrival,
+        arrival_output_path
+    )
+
+    # Arrays of departure/arrival times and states
+    et_departures, states_depart = stateReader(departure_output_path)
+    et_arrivals, states_arrive   = stateReader(arrival_output_path)
 
     # Number of days in each array and total combinations
+    ds  = len( et_departures )
+    as_ = len( et_arrivals   )
+    total = ds * as_
 
+    print( 'Departure days: %i.'     % ds    )
+    print( 'Arrival days: %i.'       % as_   )
+    print( 'Total Combinations: %i.' % total )
 
-    # Create empty array for C3, v_infinity, and tof
-
+    # Create empty array for C3, v_infinity, and tof to store data from lambert's solutions
+    C3_shorts     = np.zeros( (as_, ds) )
+    C3_longs      = np.zeros( (as_, ds) )
+    v_inf_shorts  = np.zeros( (as_, ds) )
+    v_inf_longs   = np.zeros( (as_, ds) )
+    tofs          = np.zeros( (as_, ds) )
 
     # Create arrays for indexing and meshgrid
-
+    x = np.arange( ds  )
+    y = np.arange( as_ )
 
     # For each combination
-
-
+    for na in y:
+        for nd in x:
 
             # State of planet0 at departure
 
