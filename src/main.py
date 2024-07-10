1fa008d71f28431e889481a95cd628439fc09281
Francisco Sanudo <francisco.sanudo10@gmail.com>
2024-07-09 21:04:49 -0700

    Updated main.py to run the interplanetary_porkchop function

--- src/main.py
+++ src/main.py
@@ -1,35 +1,40 @@
-from utils import stateReader
-from planetary_data import earth
-from ephemeris_query import *
+# Porkchop-Plot-Generator Libraries
+import planetary_data as pd
+from porkchop import interplanetary_porkchop
 
-
-# def main():
-#     # Define the filespec for the data file
-#     filespec = '../data/Earth_Ephemeris_Data.txt'
-    
-#     # Call the stateReader function
-#     julianDates, states = stateReader(filespec)
-    
-#     # Print results (or handle them as needed)
-#     print(f'\n Julian Dates: \n {julianDates}')
-#     print(f'\n\n States: \n {states}\n')
+# Main code
 
 def main():
-    # Define inputs
-    ID = earth['ID']
-    start_time = '2020-07-01'
-    stop_time = '2020-09-19'
-    step_size = '1'
+    # config parameters for porkchop plot generator
+    config = {
+        'planet0'       : pd.earth[ 'ID' ],     # Departure planet
+        'planet1'       : pd.mars[ 'ID' ],      # Target planet
+        'departure0'    : '2020-07-01',         # Intial departure date
+        'departure1'    : '2020-09-01',         # Final departure date
+        'arrival0'      : '2020-11-01',         # Initial arrival date
+        'arrival1'      : '2022-01-24',         # Final arrival date
+        'mu'            : pd.sun[ 'mu' ],       # Gravitational parameter in km**3/s**2
+        'step'          : 5,                    # Step size in days
+        'frame'         : 'J2000',              # Ecliptic of J2000
+        'observer'      : '500@0',              # Solar Sytem Barycenter
+        'cutoff_v'      : 20.0,                 # Maximum vinf to consider             
+        'c3_levels'     : None,                 # C3 levels for contour plot
+        'vinf_levels'   : None,                 # vinf levels for contour plot
+        'tof_levels'    : None,                 # tof levels for contour plot
+        'dv_levels'     : None,                 # dv levels for contour plot
+        'dv_cmap'       : 'RdPu_r',             # color map for dv contours
+        'figsize'       : ( 20, 10 ),           # figure size for contour plot
+        'lw'            : 1.5,                  # linewidth for contour lines
+        'title'         : 'Porkchop Plot',      # Plot title
+        'fontsize'      : 15,                   # Axes fontsize
+        'show'          : True,                # For displaying the figure
+        'filename'      : None,                 # Specify filename for c3 plot
+        'filename_dv'   : None,                 # Specify filename for dv plot
+        'dpi'           : 300,                  # Specify target dpi
+        'load'          : False                 #?
+    }
 
-    # Call function
-    url = generate_url(ID, start_time, stop_time, step_size)
-
-    # Define relative path for the output file
-    output_dir = '../data/departure_data/'
-    output_filename = f"ephemeris_data_{start_time}_{stop_time}.txt"
-    output_path = output_dir + output_filename
-
-    save_query_to_file(url, output_path)
+    interplanetary_porkchop( config )
 
 if __name__ == "__main__":
     main()
